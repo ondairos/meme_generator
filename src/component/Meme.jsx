656291a7b1d055aa02ee4d21273853b5bf2c033a
Ondairos <ondairos@gmail.com>
2022-12-13 12:54:33 +0200

    added meme html and css

--- src/component/Meme.jsx
+++ src/component/Meme.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export const Meme = () => {
   return (
-    <div>Meme</div>
-  )
-}
+    <div className="meme__container">
+      <input className="meme__input1" type="text" value={"Take my Money"} />
+      <input className="meme__input2" type="text" value={"Monkas"} />
+
+      <div className="meme_buttonContainer">
+        <button className="meme__button" type="submit">
+          Get a new meme image
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Meme;

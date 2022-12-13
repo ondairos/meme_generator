f8fcd81060a3e8e58b9695af91a3fddeb86bee3b
Ondairos <ondairos@gmail.com>
2022-12-13 13:08:18 +0200

    more styles to meme form

--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 ## Meme Generator Created with React JS
 
+Used API:Image Flip
+
 ## commands:
 To build: npm install
 to run: npm run dev
--- src/component/Meme.jsx
+++ src/component/Meme.jsx
@@ -3,14 +3,18 @@ import React from "react";
 export const Meme = () => {
   return (
     <div className="meme__container">
-      <input className="meme__input1" type="text" value={"Take my Money"} />
-      <input className="meme__input2" type="text" value={"Monkas"} />
+      <form action="">
+        <input
+          className="meme__input1"
+          type="text"
+          placeholder="Take my Money"
+        />
+        <input className="meme__input2" type="text" placeholder="Monkas" />
 
-      <div className="meme_buttonContainer">
         <button className="meme__button" type="submit">
           Get a new meme image
         </button>
-      </div>
+      </form>
     </div>
   );
 };

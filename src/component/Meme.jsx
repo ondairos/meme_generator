36f14906276af71f16fce15fa4029290220845e6
Ondairos <ondairos@gmail.com>
2022-12-14 12:19:46 +0200

    created two states for displaying memes

--- src/component/Meme.jsx
+++ src/component/Meme.jsx
@@ -1,19 +1,36 @@
 import React from "react";
-import memesData from "../memesData.js"
+import memesData from "../memesData.js";
 
 export const Meme = () => {
+  const [meme, setMeme] = React.useState({
+    // create object
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  // state with all the memes
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
+  function handleMeme() {
+    const memesArray = allMemeImages.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const imageUrl = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: imageUrl,
+    }));
+  }
+
   return (
     <div className="meme__container">
-        <input
-          className="meme__input1"
-          type="text"
-          placeholder="Take my Money"
-        />
-        <input className="meme__input2" type="text" placeholder="Monkas" />
+      <input className="meme__input1" type="text" placeholder="Take my Money" />
+      <input className="meme__input2" type="text" placeholder="Monkas" />
 
-        <button className="meme__button" type="submit">
-          Get a new meme image
-        </button>
+      <button className="meme__button" onClick={handleMeme}>
+        Get a new meme image
+      </button>
+      <img src={meme.randomImage} alt="" className="meme__image" />
     </div>
   );
 };

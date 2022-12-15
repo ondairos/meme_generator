72547fa0e4f7c31ae2bb6d98c6f74575d3443b87
Ondairos <ondairos@gmail.com>
2022-12-15 13:13:53 +0200

    added input text functionality with state

--- src/component/Meme.jsx
+++ src/component/Meme.jsx
@@ -22,18 +22,42 @@ export const Meme = () => {
     }));
   }
 
+  // handle change function as soon as keypresses inside the input we change state with the new value
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
+  // Computed property names [name] are a useful feature of JavaScript that can make your code more concise and easier to read. They allow you to use a variable as a property name, which can make your code more dynamic and easier to maintain.
+
   return (
     <div className="meme__container">
-      <input className="meme__input1" type="text" placeholder="Take my Money" />
-      <input className="meme__input2" type="text" placeholder="Monkas" />
+      <input
+        className="meme__input1"
+        type="text"
+        placeholder="One does not simply..."
+        name="topText"
+        onChange={handleChange}
+        value={meme.topText}
+      />
+      <input
+        className="meme__input2"
+        type="text"
+        placeholder="walk into mordor"
+        name="bottomText"
+        onChange={handleChange}
+        value={meme.bottomText}
+      />
 
       <button className="meme__button" onClick={handleMeme}>
         Get a new meme image
       </button>
       <div className="meme">
         <img src={meme.randomImage} alt="" className="meme__image" />
-        <h2 className="meme--text top">One does not simply</h2>
-        <h2 className="meme--text bottom">Walk into Mordor</h2>
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
       </div>
     </div>
   );
